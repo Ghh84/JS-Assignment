85c81bfc98d0a00e45ded94518066e0521c394e3
Ghh84 <ghhiwethh@gmail.com>
2022-12-08 21:32:16 +0200

    add function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // 1. Add function code goes here
-exports.add = function add() {};
+exports.add = function add(num1, num2) {
+  return num1+num2;
+};
 
 // 2. Multiply function code goes here
 exports.multiply = function multiply() {};
